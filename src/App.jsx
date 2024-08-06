b6ef62a905e53974eccc75745f9b36d8242f4db0
carlainha <carolradichi23@gmail.com>
2024-08-06 10:51:48 -0300

    Aula dia 06/08

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import {Post} from "./Post"
-import { Header } from "./components/Header"
-import './global.css'
+import {Post} from "./Post";
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
+import './global.css';
 
 export function App() {
 return (
   <div>
     <Header/>
-    <Post 
-      author="Machado de Assis"
-      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
-       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
-    <Post 
-      author="Clarice Lispector"
-      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
-       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
-    <Post 
-      author="Jane Austin"
-      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
-       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+    <div className={styles.wrapper}>
+      <aside>
+        Sidebar
+      </aside>
+      <main>
+        <Post 
+          author="Machado de Assis"
+          coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+          voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+        <Post 
+          author="Clarice Lispector"
+          coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+          voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+        <Post 
+          author="Jane Austin"
+          coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+          voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+      </main>
+    </div>
   </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css';
+import logo from '../assets/logo.png';
 
 export function Header(){
     return(
         <header className={styles.header}>
-            <strong>Etec Cidade do Livro</strong>
+            <img src={logo} alt="logo" />
         </header>
     );
 }
